7bb6527cbb2088dd285a54118a812eed87819862
Trevor Ackerman <trevorackerman.softdev@gmail.com>
2015-11-27 20:00:18 -0700

    Update url's for outerspatial, otherwise safari browser fails to pull their data

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@ var mapCenter = [40.00605, -105.26413];
 var defaultZoom = 12;
 
 var appId = "6"; // OuterSpatial assigns a unique id for each customer
-var url = "http://www.outerspatial.com";
-var trailsyBaseEndpoint = url + '/api/v0/applications/' + appId;
+var url = "http://api.outerspatial.com";
+var trailsyBaseEndpoint = url + '/v0/applications/' + appId;
 
 module.exports = {
   trailEndpoint: trailsyBaseEndpoint + '/cached_trails_csv',
