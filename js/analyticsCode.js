2909400995be9d37d662901e63792520aeb46eea
Josh Kalov <jkalov@gmail.com>
2017-03-21 23:56:28 -0500

    function to send jquery-address analytics updates to 2 trackers

--- js/analyticsCode.js
+++ js/analyticsCode.js
@@ -1,6 +1,7 @@
 'use strict'
 var $ = require('jquery')
 var Config = require('./config.js')
+require('jquery-address')
 
 var setup = function () {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -10,13 +11,23 @@ var setup = function () {
 
   ga('create', Config.gaCode1, 'auto', 'tracker1')
   ga('tracker1.set', 'anonymizeIp', true);
-  ga('tracker1.send', 'pageview');
+  //ga('tracker1.send', 'pageview');
   if (Config.gaCode2) {
     ga('create', Config.gaCode1, 'auto', 'tracker2')
     ga('tracker2.set', 'anonymizeIp', true);
-    ga('tracker2.send', 'pageview');
+    //ga('tracker2.send', 'pageview');
   }
 }
+
+var sendPageView = function (value) {
+  // console.log('sendPageView value = ' + value)
+  ga('tracker1.send', 'pageview', value)
+  if (Config.gaCode2) {
+    ga('tracker2.send', 'pageview', value)
+  }
+}
+
+$.address.tracker(sendPageView)
 
 var trackClickEventWithGA = function (category, action, label) {
   ga('tracker1.send', {
