26b3b94d324bcf26a5bc952f696661ea3b437824
GISDEV <gisdev-GeographicInformationSystems@cookcounty.onmicrosoft.com>
2021-07-08 10:04:30 -0500

    Don't show trails for an accessible search result. Issue #261

--- js/trailExcludes.js
+++ js/trailExcludes.js
@@ -1,6 +1,6 @@
 // Which tags should NOT should up in the results list
 var tagsExcludeTrailsList = []
-tagsExcludeTrailsList = tagsExcludeTrailsList.concat(['boat rental', 'canoe rental', 'kayak rental', 'boating center', 'boat_rental', 'camping', 'camp', 'campground', 'canoeing', 'kayaking', 'boating', 'canoe', 'kayak', 'boat', 'boat_ramp', 'disc golf', 'frisbee', 'disc_golf', 'dog park', 'off-leash dog', 'dog_friendly', 'drone flying', 'drone', 'fishing', 'golfing', 'golf', 'ice fishing', 'ice_fishing', 'ice skating', 'ice skate', 'skating_ice', 'indoor event space', 'wedding', 'meeting', 'indoor_rental', 'model airplane flying', 'm_airplane', 'model boat ', 'm_boat', 'nature center', 'museum', 'education', 'nature_center', 'picnic / event space', 'grove', 'bbq', 'grill', 'picnic_grove + shelter', 'sled', 'coasting', 'sledding', 'snowmobiling', 'snowmobile', 'snowmachine', 'pool', 'aquatic', 'swimming', 'volunteering', 'restoration', 'volunteer', 'zip line / treetop adventure', 'zip_line'])
+tagsExcludeTrailsList = tagsExcludeTrailsList.concat(['accessible', 'boat rental', 'canoe rental', 'kayak rental', 'boating center', 'boat_rental', 'camping', 'camp', 'campground', 'canoeing', 'kayaking', 'boating', 'canoe', 'kayak', 'boat', 'boat_ramp', 'disc golf', 'frisbee', 'disc_golf', 'dog park', 'off-leash dog', 'dog_friendly', 'drone flying', 'drone', 'fishing', 'golfing', 'golf', 'ice fishing', 'ice_fishing', 'ice skating', 'ice skate', 'skating_ice', 'indoor event space', 'wedding', 'meeting', 'indoor_rental', 'model airplane flying', 'm_airplane', 'model boat ', 'm_boat', 'nature center', 'museum', 'education', 'nature_center', 'picnic / event space', 'grove', 'bbq', 'grill', 'picnic_grove + shelter', 'sled', 'coasting', 'sledding', 'snowmobiling', 'snowmobile', 'snowmachine', 'pool', 'aquatic', 'swimming', 'volunteering', 'restoration', 'volunteer', 'zip line / treetop adventure', 'zip_line'])
 
 // Which tags should NOT should up on map
 var tagsExcludeTrailsMap = []
@@ -8,6 +8,8 @@ tagsExcludeTrailsMap = tagsExcludeTrailsMap.concat(['drone'])
 tagsExcludeTrailsMap = tagsExcludeTrailsMap.concat(['m_airplane'])
 tagsExcludeTrailsMap = tagsExcludeTrailsMap.concat(['snowmobile', 'snowmachine'])
 tagsExcludeTrailsMap = tagsExcludeTrailsMap.concat(['swimming', 'swim', 'pool', 'aquatic'])
+
+
 
 module.exports = {
   list: tagsExcludeTrailsList,
