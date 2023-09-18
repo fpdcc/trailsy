aaff18f3061ea91314b125e84cdba24d79147d1c
GISDEV <gisdev-GeographicInformationSystems@cookcounty.onmicrosoft.com>
2023-09-18 03:13:41 -0500

    fall colors tweaks

--- js/panelFunctions.js
+++ js/panelFunctions.js
@@ -645,6 +645,11 @@ var panelFuncs = function (map) {
 
         // Activities/Amenities NOT on map
 
+        // fall_colors = Fall Colors Hotspot
+        if (poi.properties.tags[':panel'].indexOf('fall_colors') > -1) {
+          fpccAmenitiesString += "<div class='fpccAmenity'><svg class='icon icon-fall-colors'><use xlink:href='icons/defs.svg#icon-fall-colors'></use></svg><span class='fpccAmenityTitle'>Fall Colors Hotspot</span></div>"
+        }
+
         // birding = Birding Hotspot
         if (poi.properties.tags[':panel'].indexOf('birding') > -1) {
           fpccAmenitiesString += "<div class='fpccAmenity'><svg class='icon icon-birding-hotspot'><use xlink:href='icons/defs.svg#icon-birding-hotspot'></use></svg><span class='fpccAmenityTitle'><a href='https://fpdcc.com/things-to-do/birding/'>Birding Hotspot</a></span></div>"
@@ -688,12 +693,6 @@ var panelFuncs = function (map) {
         if (poi.properties.tags[':panel'].indexOf('equestrian') > -1) {
           fpccAmenitiesString += "<div class='fpccAmenity'><svg class='icon icon-equestrian'><use xlink:href='icons/defs.svg#icon-equestrian'></use></svg><span class='fpccAmenityTitle'><a href='https://fpdcc.com/things-to-do/equestrian/'>Equestrian Parking</a></span></div>"
         }
-
-        // fall_colors = Fall Colors Hotspot
-        if (poi.properties.tags[':panel'].indexOf('fall_colors') > -1) {
-          fpccAmenitiesString += "<div class='fpccAmenity'><svg class='icon icon-fall-colors'><use xlink:href='icons/defs.svg#icon-fall-colors'></use></svg><span class='fpccAmenityTitle'>Fall Colors Hotspot</span></div>"
-        }
-
 
         // fishing = Fishing
         if (poi.properties.tags[':panel'].indexOf('fishing') > -1) {
