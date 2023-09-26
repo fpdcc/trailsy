e3416d70824d05b9c1faa5b53a59d908764b144f
GISDEV <gisdev-GeographicInformationSystems@cookcounty.onmicrosoft.com>
2023-09-26 16:00:05 -0500

    remove testing note

--- js/panelFunctions.js
+++ js/panelFunctions.js
@@ -647,7 +647,7 @@ var panelFuncs = function (map) {
 
         // birding = Birding Hotspot
         if (poi.properties.tags[':panel'].indexOf('birding') > -1) {
-          fpccAmenitiesString += "<div class='fpccAmenity'><svg class='icon icon-birding-hotspot'><use xlink:href='icons/defs.svg#icon-birding-hotspot'></use></svg><span class='fpccAmenityTitle'><a href='https://fpdcc.com/things-to-do/birding/'>Birding Hotspot testing</a></span></div>"
+          fpccAmenitiesString += "<div class='fpccAmenity'><svg class='icon icon-birding-hotspot'><use xlink:href='icons/defs.svg#icon-birding-hotspot'></use></svg><span class='fpccAmenityTitle'><a href='https://fpdcc.com/things-to-do/birding/'>Birding Hotspot</a></span></div>"
         }
         // fall_colors = Fall Color Hotspots
         if (poi.properties.tags[':panel'].indexOf('fall_colors') > -1) {
