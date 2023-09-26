c812d3806a6573feef494bbcb190b468d4f4847b
GISDEV <gisdev-GeographicInformationSystems@cookcounty.onmicrosoft.com>
2023-09-26 15:57:25 -0500

    add fall colors

--- js/panelFunctions.js
+++ js/panelFunctions.js
@@ -647,7 +647,11 @@ var panelFuncs = function (map) {
 
         // birding = Birding Hotspot
         if (poi.properties.tags[':panel'].indexOf('birding') > -1) {
-          fpccAmenitiesString += "<div class='fpccAmenity'><svg class='icon icon-birding-hotspot'><use xlink:href='icons/defs.svg#icon-birding-hotspot'></use></svg><span class='fpccAmenityTitle'><a href='https://fpdcc.com/things-to-do/birding/'>Birding Hotspot</a></span></div>"
+          fpccAmenitiesString += "<div class='fpccAmenity'><svg class='icon icon-birding-hotspot'><use xlink:href='icons/defs.svg#icon-birding-hotspot'></use></svg><span class='fpccAmenityTitle'><a href='https://fpdcc.com/things-to-do/birding/'>Birding Hotspot testing</a></span></div>"
+        }
+        // fall_colors = Fall Color Hotspots
+        if (poi.properties.tags[':panel'].indexOf('fall_colors') > -1) {
+          fpccAmenitiesString += "<div class='fpccAmenity'><svg class='icon icon-fall-colors'><use xlink:href='icons/defs.svg#icon-fall-colors'></use></svg><span class='fpccAmenityTitle'>Fall Color Hotspot</span></div>"
         }
         // cycling = Bicycling
         if (poi.properties.tags[':panel'].indexOf('cycling') > -1) {
